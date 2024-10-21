d343a85acab5aa0553f48273eb5a0f5150097352
Talentrise Technokrate Pvt Ltd <github@technokrate.com>
2024-10-21 16:49:52 +0530

    Update index.js added cors code

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tenderRoutes = require('./routes/tenderRoutes'); 
 const dotenv = require('dotenv');
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // Allow all origins (for development purposes; adjust for production)
+  })
+);
 app.use(express.json());
 app.use('/tenders', tenderRoutes);
 
