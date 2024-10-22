c33dc395288cb245b58b31c82f67555c6ba64b15
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2024-10-22 15:41:11 +0530

    added getVendorBy ID api

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { create, getAllVendors, findVendor, updateVendor } = require("../controllers/vendorController.js");
+const { create, getAllVendors, findVendor, updateVendor, getVendorById } = require("../controllers/vendorController.js");
 
 const route = express.Router();
 
 route.post("/create", create);
 route.get("/getAllVendors", getAllVendors);
 route.get("/getByQuery", findVendor);
+route.get("/getVendorById/:id", getVendorById);
 route.put("/update/:id", updateVendor);
 
 module.exports = route;
