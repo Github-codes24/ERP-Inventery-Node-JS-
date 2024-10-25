0fe92a582f109f304e999d78488019eaa95d41e7
sanidhya290 <sanidhya02neema@gmail.com>
2024-10-25 16:09:47 +0530

    purchase order api code

--- controllers/purchaseOrderController.js
+++ controllers/purchaseOrderController.js
@@ -58,20 +58,77 @@ const createPurchaseOrder = async (req, res) => {
     await newPurchaseOrder.save();
 
     
-    res.status(201).json({
+    return res.status(201).json({
       message: 'Purchase Order created successfully!',
       purchaseOrder: newPurchaseOrder
     });
   } catch (error) {
     
-    res.status(400).json({
+    return res.status(500).json({
       message: 'Error creating purchase order',
       error: error.message
     });
   }
 };
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await PurchaseOrder.find(); 
+    return res.status(200).json(orders); 
+  } catch (error) {
+    return res.status(500).json({ message: 'Error fetching orders', error });
+  }
+};
+const getOrderById = async (req, res) => {
+  try {
+    const order = await PurchaseOrder.findById(req.params.id);
+    
+    if (!order) {
+      return res.status(404).json({ message: 'Purchase order not found' });
+    }
+
+    return res.status(200).json(order); // Send the order data in response
+  } catch (error) {
+    return res.status(500).json({ message: 'Error fetching order', error });
+  }
+};
+
+const updateOrderById = async (req, res) => {
+  try {
+    const updatedOrder = await PurchaseOrder.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },  
+      { new: true, runValidators: true } 
+    );
+
+    if (!updatedOrder) {
+      return res.status(404).json({ message: 'Purchase order not found' });
+    }
+
+   return res.status(200).json(updatedOrder); e
+  } catch (error) {
+    return res.status(500).json({ message: 'Error updating order', error });
+  }
+};
+const findOrdersByParams = async (req, res) => {
+  try {
+    const filters = req.query; 
+    const orders = await PurchaseOrder.find(filters);
+    
+    if (orders.length === 0) {
+      return res.status(404).json({ message: 'No orders found matching the criteria' });
+    }
+
+   return  res.status(200).json(orders); 
+  } catch (error) {
+    return res.status(500).json({ message: 'Error finding orders', error });
+  }
+};
 
 module.exports = {
-  createPurchaseOrder
+  createPurchaseOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrderById,
+  findOrdersByParams
 };
 
