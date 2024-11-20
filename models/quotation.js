5499954f2579d5eabd9fe65ce1773276a97ee968
Siddhantji <teotiasiddhant@gmail.com>
2024-11-20 13:50:14 +0530

    modified: quotations

--- models/quotation.js
+++ models/quotation.js
@@ -1,52 +1,48 @@
 const mongoose = require("mongoose");
 
 const QuotationSchema = new mongoose.Schema({
-  
-  quotationNo: { type: String, required: true, unique: true },
+  quotationNo: { type: String, unique: true },
   quotationName: { type: String },
-  quotationDate: { type: Date, required: true },
+  quotationDate: { type: Date },
   validity: { type: Date },
 
   from: {
-    companyName: { type: String, required: true },
-    address: { type: String, required: true },
-    cityStateZip: { type: String },
-    email: { type: String, required: true },
-    mobile: { type: String, required: true },
-  },
-
-  
-  to: {
-    customerName: { type: String, required: true },
-    address: { type: String, required: true },
+    companyName: { type: String },
+    address: { type: String },
     cityStateZip: { type: String },
     email: { type: String },
     mobile: { type: String },
   },
 
-  
-  bankDetails: {
-    bankName: { type: String, required: true },
-    location: { type: String },
-    accountName: { type: String, required: true },
-    accountType: { type: String, required: true },
-    ifscCode: { type: String, required: true },
+  to: {
+    customerName: { type: String },
+    address: { type: String },
+    cityStateZip: { type: String },
+    email: { type: String },
+    mobile: { type: String },
   },
 
-  
+  bankDetails: {
+    bankName: { type: String },
+    location: { type: String },
+    accountName: { type: String },
+    accountType: { type: String },
+    ifscCode: { type: String },
+  },
+
   items: [
     {
-      itemNo: { type: Number, required: true },
-      description: { type: String},
-      quantity: { type: Number, required: true },
-      unitPrice: { type: Number, required: true },
+      itemNo: { type: Number },
+    //  description: { type: String }, // this field is not available in figma
+      quantity: { type: Number },
+      unitPrice: { type: Number },
       discountPercentage: { type: Number, default: 0 },
       discountAmount: { type: Number, default: 0 },
       sgstRate: { type: Number, default: 0 },
       sgstAmount: { type: Number, default: 0 },
       cgstRate: { type: Number, default: 0 },
       cgstAmount: { type: Number, default: 0 },
-      total: { type: Number, required: true },
+      total: { type: Number },
     },
   ],
 
@@ -56,11 +52,15 @@ const QuotationSchema = new mongoose.Schema({
     paymentTerms: { type: String },
   },
 
-  subtotal: { type: Number, required: true },
+  subtotal: { type: Number },
   totalDiscountPercentage: { type: Number, default: 0 },
   totalDiscountAmount: { type: Number, default: 0 },
   taxes: { type: Number, default: 0 },
-  netAmount: { type: Number, required: true },
+  netAmount: { type: Number },
+  isDeleted:{
+    type:Boolean,
+    default:false
+  }
 });
 
 module.exports = mongoose.model("Quotation", QuotationSchema);
