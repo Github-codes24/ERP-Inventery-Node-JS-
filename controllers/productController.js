bd36086726e895d4cb925a8dfa84187baa898a1c
ankit <ankitkarapurkar444@gmail.com>
2024-11-25 22:48:29 -0800

    lates

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,18 @@
 const Product = require('../models/productModel');
 
 
+
+const getNewSrNumber = async (req,res) => {
+  try {
+      const clients = await Product.find();
+      const currentSrNo = clients.length + 1;
+      return res.status(200).json({ success: true, srNo: currentSrNo });
+  } catch (error) {
+      console.error("Error getting new sr number:", error);
+      return 1;
+  }
+}
+
 // Get products by name
 const getProducts = async (req, res) => {
   try {
@@ -245,5 +257,6 @@ module.exports = {
     // getTopSellingProducts,
     getRequiredEmergencyProducts,
     getProductDetails,
-    getStockNames
+    getStockNames,
+    getNewSrNumber
 };
