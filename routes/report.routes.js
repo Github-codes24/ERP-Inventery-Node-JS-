dc9b2e6a247fcd572ab004feb6245c92e78f48e6
Siddhantji <teotiasiddhant@gmail.com>
2024-12-03 13:11:35 +0530

    fixed: report.routes

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/report.controller');
-const companyMiddleware = require("../middleware/companyMiddleware");
 
 router.get('/salesByCompany', reportController.salesByCompany);
 router.get('/salesByProduct', reportController.salesByProduct);
 router.get('/salesByRegion', reportController.salesByRegion);
 router.get('/saleBySalesPerson', reportController.saleBySalesPerson);
 router.get('/salesByDistributor', reportController.salesByDistributor);
+module.exports = router;
